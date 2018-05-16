3957224227e0b523cbbaec9fe5efbd9880fab26e
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2018-05-16 18:03:52 +0100

    Created class Book, small CSS changes, created resgiter modal

--- index.js
+++ index.js
@@ -118,10 +118,10 @@ class Library{
 }
 
 // CLASS CATEGORIA
-class Categoria{
+class Category{
     constructor(nameCategory){
         this.nameCategory = nameCategory;
-        this._categoryId = Categoria.generateId();
+        this._categoryId = Category.generateId();
     }
 
     // NAMECATEGORY PROPERTY
@@ -148,6 +148,177 @@ class Categoria{
 
 
 }
+
+// CLASS TAG
+class Tag{
+    constructor(nameTag){
+        this.nameTag = nameTag;
+        this._tagId = Tag.generateId();
+    }
+
+    // NAMETAG PROPERTY
+    set nameTag(newTag){
+        this._nameTag = newTag;
+    }
+    get nameTag(){
+        return this._nameTag;
+    }
+
+    // ID PROPERTY
+    get tagId(){
+        return this._tagId;
+    }
+
+    // ID GENERATOR
+    static generateId(){
+        function rand() {
+            return Math.floor(( Math.random() + 1) * 0x10000).toString(16).substring(1);
+        }
+        return rand() + rand() + '-' + rand() + '-' + rand() + '-' + rand() + '-' + rand() + rand() + rand();
+    }
+
+
+
+}
+
+// CLASS BOOK
+class Book{
+    constructor(title, cover, autor, synopsis, releaseDate, category, tags, publisher, numberPages, condition, donerName, donationDate, libraryId){
+    this.title = title;
+    this.cover = cover;
+    this.autor = autor;
+    this.synopsis = synopsis;
+    this.releaseDate = releaseDate;
+    this.category = category;
+    this.tags = tags;
+    this.publisher = publisher;
+    this.numberPages = numberPages;
+    this.condition = condition;
+    this.donerName = donerName;
+    this.donationDate = donationDate;
+    this.libraryId = libraryId;
+    this._bookId = Book.generateId();
+    }
+
+    
+    // TITLE PROPERTY
+    set title(newTitle){
+        this._title = newTitle;
+    }
+    get title(){
+        return this._title;
+    }
+
+    // COVER PROPERTY
+    set cover(newCover){
+        this._cover = newCover;
+    }
+    get cover(){
+        return this._cover;
+    }
+
+    // AUTOR PROPERTY
+    set autor(newAutor){
+        this._autor = autor;
+    }
+    get autor(){
+        return this._autor;
+    }
+
+    // SYNOPSIS PROPERTY
+    set synopsis(newSynopsis){
+        this._synopsis = newSynopsis;
+    }
+    get synopsis(){
+        return this._synopsis;
+    }
+
+    // RELEASEDATE PROPERTY
+    set releaseDate(newDate){
+        this._releaseDate= newDate;
+    }
+    get releaseDate(){
+        return this._releaseDate;
+    }
+
+    // CATEGORY PROPERTY
+    set category(newCategory){
+        this._category= newCategory;
+    }
+    get category(){
+        return this._category;
+    }
+
+    // TAGS PROPERTY
+    set tags(newTags){
+        this._tags= newTags;
+    }
+    get tags(){
+        return this._tags;
+    }
+
+    // PUBLISHER PROPERTY
+    set publisher(newPublisher){
+        this._publisher= newPublisher;
+    }
+    get publisher(){
+        return this._publisher;
+    }
+
+    // NUMBERPAGES PROPERTY
+    set numberPages(newNumber){
+        this._numberPages= newNumber;
+    }
+    get numberPages(){
+        return this._numberPages;
+    }
+
+    // CONDITION PROPERTY
+    set condition(newCondition){
+        this._condition= newCondition;
+    }
+    get condition(){
+        return this._condition;
+    }
+
+    // DONERNAME PROPERTY
+    set donerName(newDoner){
+        this._donerName= newDoner;
+    }
+    get donerName(){
+        return this._donerName;
+    }
+
+    // DONATIONDATE PROPERTY
+    set donationDate(newDate){
+        this._donationDate= newDate;
+    }
+    get donationDate(){
+        return this._donationDate;
+    }
+
+    // LIBRARYID PROPERTY
+    set libraryId(newId){
+        this._libraryId= newId;
+    }
+    get libraryId(){
+        return this._libraryId;
+    }
+
+    // ID PROPERTY
+    get bookId(){
+        return this._bookId;
+    }
+
+    // ID GENERATOR
+    static generateId(){
+        function rand() {
+            return Math.floor(( Math.random() + 1) * 0x10000).toString(16).substring(1);
+        }
+        return rand() + rand() + '-' + rand() + '-' + rand() + '-' + rand() + '-' + rand() + rand() + rand();
+    }
+}
+
 
 window.onload = function () {
     // TEST
