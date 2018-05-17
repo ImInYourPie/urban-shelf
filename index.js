8fad4dba231d6abd5e7e1d80c1dd4dde851b0b3e
Diogo Miguel Melo Magalhães <iminyourpie@gmail.com>
2018-05-17 20:05:58 +0100

    Started admin pages, css admin page, rest is easy, other changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// ARRAYS
+let arrayUsers = [];
+let arrayTags = [];
+let arrayCategorias = [];
+let arrayLivros = [];
+
 // CLASS USER
 class User{
     constructor(username, password, email, userType){
@@ -322,8 +328,74 @@ class Book{
 
 window.onload = function () {
     // TEST
-     let user1 = new User("Miguel","mutts30", "d.miguel.melo@gmail.com", 0);
-     console.log(user1.userId);
-     console.log(Math.floor(( Math.random() + 1) * 0x10000).toString(16));
+    let admin1 = new User("Miguel","mutts30", "d.miguel.melo@gmail.com", 0);
+    arrayUsers.push(admin1);
+    // console.log(user1.userId);
+    // console.log(Math.floor(( Math.random() + 1) * 0x10000).toString(16));
+
+    // VARIABLES
+    let navLogin = document.getElementById("navLogin");
+    let navRegister = document.getElementById("navRegister");
+    let navHi = document.getElementById("navHi");
+    let navLogout = document.getElementById("navLogout");
+    let formLogin = document.getElementById("formLogin");
+
+    // ESCONDER OPÇOES AUTENTICAÇAO
+    navHi.style.display = "none";
+    navLogout.style.display = "none";
+
+    // LOGIN SUBMIT
+    formLogin.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        // 1.VARIABLES
+        let inputUsernameLogin = document.getElementById("loginUsername").value;
+        let inputPasswordLogin = document.getElementById("inputPassword").value;
+        // 2.VERIFICAR SE UTILIZADOR EXISTE
+        let userExists = false;
+        let userName = "";
+        let id = "";
+        let typeUser = 0;
+        for (let i = 0; i < arrayUsers.length; i++) {
+            if (arrayUsers[i].email == inputUsernameLogin && arrayUsers[i].password == inputPasswordLogin) {
+                userExists = true;
+                id = arrayUsers[i].userId;
+                userName = arrayUsers[i].username;
+                typeUser = arrayUsers[i].userType;
+            }
+        }
+        // 3.SE EXISTE; AUTENTICAR UTILIZADOR
+        if(userExists == true){
+            // FECHAR MODAL
+            $("#loginModal").modal("hide");
+            // ALTERAR NAVBAR
+            navLogin.style.display = "none";
+            navRegister.style.display = "none";
+            navLogout.style.display = "block";
+            navHi.innerHTML = "<a id='" + id + "' class='nav-link' href='#'>Olá, " +
+            userName + "</a>";
+            navHi.style.display = "block";
+        } else{
+            alert("Dados de autenticação inválidos!");
+        }
+        
+        // CHECK USERTYPE 
+        switch (typeUser) {
+            case 0: 
+                loadAdminPage();
+                break;
+            case 1:
+                loadUserPage();
+                break;
+            case 2:
+                loadOperatorPage();
+                break;
+
+            default:
+                break;
+        }
+    })
+
+
 
 }
