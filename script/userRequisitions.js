7a17829b6fbb996aa06b034996cd2a0eedda300b
Andre-Faria-9170113 <37090430+Andre-Faria-9170113@users.noreply.github.com>
2018-06-12 10:54:39 +0100

    Added modal to userRequisitions.html and worked on the refreshing of the requisition table in userRequisitions.html/.js

--- script/userRequisitions.js
+++ script/userRequisitions.js
@@ -6,9 +6,22 @@ window.onload = function(){
     requisitionsHeader.innerHTML = navbarDropdown.innerText+requisitionsHeader.innerHTML //ATUALIZAR TITULO DA TABELA COM O NOME DO UTILIZADOR
 
     //2. ALIMENTAR TABELA COM OS LIVROS ATUALMENTE REQUISITADOS PELO USER
+    let tblRequisitions = document.getElementById("tblRequisitions")
+
+    for(let i=0; i<arrayRequisitions.length;i++){
+        if(arrayRequisitions[i]._userId == ){ //FALTA REFERENCIAR USER ATUAL //////////////////////////////////////
+            tblRequisitions.getElementsByTagName("tbody")[0].innerHTML +=  "<tr>" +
+            "<td>" + arrayRequisitions[i]._requisitionId + "</td>" +
+            "<td>" + arrayRequisitions[i]._books + "</td>" +
+            "<td>" + arrayRequisitions[i]._requisitiondDate + "</td>" + //POR DEFINIR NA CLASSE REQUISITION /////////////////////////////////////////
+            "<td>" + arrayRequisitions[i]._returnDate + "</td>" + //POR DEFINIR //////////////////////////////////////////////
+            "<td>" + arrayRequisitions[i]._multa + "</td>" + //POR DEFINIR ///////////////////////////////////////////////
+            '<td><button id="tblBtn'+i+'" type="button" class="btn btn-primary" data-toggle="modal" data-target="#bookReturnModal"> Devolver/ Pagar</button></td>' //BOTÃO PARA DEVOLVER LIVRO/ PAGAR MULTA ////////////////////////
+            +"</tr>"
+        } 
+    }
 
     //3. ATUALIZAR CONTADOR DE LIVROS REQUISITADOS
-    let tblRequisitions = document.getElementById("tblRequisitions")
     let currentRequisitions = document.getElementById("currentRequisitions")
     let tblRequisitionsBookCount = tblRequisitions.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length
     console.log(tblRequisitionsBookCount)
@@ -25,4 +38,5 @@ window.onload = function(){
     //7. ATUALIZAR TABELA DE REQUISIÇÕES
 
     //8. ATUALIZAR CONTADOR DE LIVROS REQUISITADOS
+    
 }
